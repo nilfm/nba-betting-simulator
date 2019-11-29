62c70dcf845d861ba4802808b43af4e09b265f32
nilfm <nil.fons@gmail.com>
2019-11-29 18:18:41 +0100

    Daily task, now for real

--- daily_task.py
+++ daily_task.py
@@ -2,10 +2,8 @@ import insert_games
 import insert_scores
 
 def main():
-    with open('lolaso', 'w') as f:
-        f.write("LOL")
-    #insert_scores.main()
-    #insert_games.main()
+    insert_scores.main()
+    insert_games.main()
 
 if __name__ == '__main__':
     main()
