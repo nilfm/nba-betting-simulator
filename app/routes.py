9bb486807ee7275e107694b1430cc31af18e494f
nilfm <nil.fons@gmail.com>
2019-11-29 22:58:18 +0100

    User page is now responsive on mobile

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from app import app, db
 from app.forms import LoginForm, RegistrationForm, BetForm
 from app.models import User, Game, Bet
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, jsonify, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 
