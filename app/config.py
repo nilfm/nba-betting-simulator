28ac20c7af4c497486ed4cba10535562a24b9eaa
nilfm <nil.fons@gmail.com>
2019-12-04 23:39:11 +0000

    Works in production now

--- app/config.py
+++ app/config.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 import os
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '../.env'))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you_will_never_guess'
@@ -14,7 +16,7 @@ class Config:
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['nil.fons@gmail.com']
+    ADMINS = ['nbabettingsimulator@gmail.com']
     NUM_BETS_SHOWN = 10
     NUM_RANKS_SHOWN = 10
 
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,9 @@
 from nbabetting import app
+import os
+from dotenv import load_dotenv
+
+project_folder = '/root/nba-betting-simulator'
+load_dotenv(os.path.join(project_folder, '.env'))
 
 if __name__ == '__main__':
     app.run()
