6182b62e683b862218ded10d960cea895d5a28d2
nilfm <nil.fons@gmail.com>
2019-12-07 20:06:09 +0100

    oops

--- app/routes.py
+++ app/routes.py
@@ -53,10 +53,7 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
-        return redirect(next_page)
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/logout')
@@ -255,8 +252,9 @@ def custom_key(x):
     return mapping[x['data']['category']], x['value']
 
 @app.route('/users', methods=['GET'])
-@login_required
 def users():
+    if not current_user.is_authenticated:
+        return jsonify(None)
     users = User.query.all()
     params = [
         {
