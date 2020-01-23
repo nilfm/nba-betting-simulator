c22570d083228e3cc7a062654da3e9353e53e38d
nilfm <nil.fons@gmail.com>
2020-01-23 13:07:26 +0100

    ?

--- app/static/js/flashed_messages.js
+++ app/static/js/flashed_messages.js
@@ -13,11 +13,15 @@ var flashed_messages = new Vue({
                 .then((messages_json) => {
                     this.messages = messages_json;
                 })
+        },
+        clear_flashed_messages: function() {
+            this.messages = [];
         }
     }
 })
 
 flashed_messages.get_flashed_messages();
+setTimeout(flashed_messages.clear_flashed_messages, 10000);
 //setInterval(flashed_messages.get_flashed_messages, 60000);
 
 
